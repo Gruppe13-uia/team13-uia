0d7b6001d730b6130ec89c58b5c74cc1a7c08b76
rnldmp <69900877+rnldmp@users.noreply.github.com>
2020-10-01 13:01:23 +0200

    Update setup.sh
    Fixa mariadb

--- full-web-skeleton/setup.sh
+++ full-web-skeleton/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start database
-docker run --rm --name mariadb -p 3308:3306/tcp -v $(pwd)/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=12345 -d mariadb
+docker run --rm --name mariadb -p 3308:3306/tcp -v $(pwd)/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=12345 -d mariadb 10.5.4
 
 # Start maven-cache
 docker volume create --name maven-repo
